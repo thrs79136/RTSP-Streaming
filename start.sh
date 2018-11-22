f3470edf41fa231021cc9c04ad92b1e890274a08
Jörg Vogt <joerg.vogt@htw-dresden.de>
2018-11-22 12:27:59 +0100

    Startscript erweitert

--- start.sh
+++ start.sh
@@ -4,15 +4,18 @@
 # für Eclipse sind die Verzeichnisse entsprechend anzupassen
 
 port=3333
+video=videos/htw.mjpeg
 src=src
 bin=out/production/RTSP-Streaming
 
 
 # Kompilierung
-javac -cp src src/Server.java  -d out/production/RTSP-Streaming
-javac -cp src src/Client.java  -d out/production/RTSP-Streaming
+echo "compile classes..."
+javac -cp $src ${src}/Server.java  -d $bin 
+javac -cp $src ${src}/Client.java  -d $bin 
 
 # Start
-java -cp out/production/RTSP-Streaming  Server $port &
+echo "start classes..."
+java -cp $bin  Server $port &
 sleep 1s
-java -cp out/production/RTSP-Streaming  Client localhost $port videos/htw.mjpeg &
+java -cp $bin  Client localhost $port $video &
